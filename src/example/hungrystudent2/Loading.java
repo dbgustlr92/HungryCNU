60cd83e8feb78611b787d1c73d9c63f1b998b413
dbgustlr92 <dbgustlr92@naver.com>
2015-02-16 14:45:03 +0900

    인트로화면 추가!

---
+++ src/example/hungrystudent2/Loading.java
@@ -0,0 +1,25 @@
+package example.hungrystudent2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class Loading extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading);
+		
+		Handler handler = new Handler(){
+			public void handleMessage(Message msg){
+				Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		};
+		handler.sendEmptyMessageDelayed(0, 3000);
+	}
+}
